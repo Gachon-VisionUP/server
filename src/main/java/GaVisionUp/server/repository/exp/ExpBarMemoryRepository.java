72c7aacc53aaec66b6758e39d60c10ae98ac8436
spenshark <xzx01299@naver.com>
2025-01-07 22:36:19 +0900

    ✨ [feature]#4 Role enum 추가 및 Department enum 수정

--- src/main/java/GaVisionUp/server/repository/exp/ExpBarMemoryRepository.java
+++ src/main/java/GaVisionUp/server/repository/exp/ExpBarMemoryRepository.java
@@ -9,7 +9,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 
-@Repository
+//@Repository
 public class ExpBarMemoryRepository implements ExpBarRepository {
 
     private final Map<Long, ExpBar> store = new HashMap<>();
